25303a0fc27bcfe9738049eb16899dc6076f37ad
Javier García Ibáñez <i82gaibj@uco.es>
2022-03-05 15:24:12 +0100

    Modificación del numero de objetos

--- Knapsack.py
+++ Knapsack.py
@@ -43,9 +43,15 @@ def aplicarOperadoresGeneticos(poblacion, k, cProb, mProb):
     return poblacion #Devolver la nueva poblacion (sin evaluar)
 
 def main():
-    pesos = [ 34, 45, 14, 76, 32, 61, 37, 54, 23, 90 ]
-    precios = [ 340, 210, 87, 533, 112, 427, 260, 356, 145, 637 ]
-    pesoMax = 100 #Peso máximo que se puede poner en la mochila
+    
+    pesos = [ 34, 45, 14, 76, 32 ] #Para 5 objetos
+    precios = [ 340, 210, 87, 533, 112 ] #Para 5 objetos
+    pesoMax = 100 #Peso máximo que se puede poner en la mochila. Para 5 objetos
+    
+    #pesos = [ 34, 45, 14, 76, 32, 61, 37, 54, 23, 90, 26, 8, 17, 41, 28, 57, 68, 19, 48, 3 ] #Para 20 objetos
+    #precios = [ 340, 210, 87, 533, 112, 427, 260, 356, 145, 637, 234, 72, 102, 358, 295, 384, 443, 123, 237, 27 ] #Para 20 objetos
+    #pesoMax = 400 #Peso máximo que se puede poner en la mochila. Para 20 objetos
+    
     nSoluciones = 25 #Tamaño de la poblacion
     maxGeneraciones = 10 #Numero de generaciones
     k = 3 #Tamaño torneo selector de padres
