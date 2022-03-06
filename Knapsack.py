722a8512e576a213f5ebe730bccd1d3aa1791fe8
Lepre lepreeeee <i82bocar@uco.es>
2022-03-06 05:11:02 +0100

    Update Knapsack.py

--- Knapsack.py
+++ Knapsack.py
@@ -18,32 +18,77 @@ def evaluarSolucion(solucion, precios, pesos, pesoMax):
 def aplicarOperadoresGeneticos(poblacion, k, cProb, mProb):
 
     #Seleccionar padres mediante torneo tamaño k
-    padres = []
-    mejor = [0,0]
 
-    for i in range(0, len(poblacion)-1):
-        for j in range(0, k):
-            randomN = random.randint(0,len(poblacion)-1)
-            padre = poblacion[randomN]   
+    mejor = [0,0]
+    cProb = cProb * 100
+
+    for i in range(len(poblacion)):
+        padres = []
+
+        for j in range(k):
+            randomN = poblacion[random.randint(0,len(poblacion)-1)]
+            padre = poblacion[randomN]
             if(padre[1] > mejor[1]):
                 mejor = padre;
             print("hola")
         padres.append(mejor);
         #print("Padre mejor", mejor)
-        
-    #Cruzar padres con probabilidad cProb
-    if random.randint(1,100) <= cProb:
+
+        mejor_valor = padres[0][1]
+        mejor_site = padres[0][0]
+
+        for j in range(k):
+            if mejor_valor < padres[i][1]:
+                mejor_valor = padres[i][1]
+                mejor_site = padres[i][0]
+
+        mejor.append(mejor_site)
+
+
+    #cruzar padres con probabilidad cprob:
+
+    size = []
+    generacion_i = []
+
+
+    for z in range (0 , len(mejor), 2):
+        padre_1 = mejor[z].copy()
+        if z+1 == len(mejor):
+            generacion_i.append(padre_1)
+            break
+
+        padre_2 = mejor[z + 1].copy()
+        if random.randint(1, 100) <=cProb:
+            cruce = random.randint(0, size-1)
+
+            for x in range(cruce, size-1):
+                aux_padre = padre_1[x]
+                padre_1[x] = padre_2[x]
+                padre_2[x] = aux_padre
+
+        generacion_i.append(padre_1)
+        generacion_i.append(padre_2)
+
+
         # vas a cruzar padres[i] con padres[i+1] y luego i+2
         # para cruzar partes los dos padre[0] por un bit aleatorio y intercambias sus partes 
 
     #Mutar padres con probabilidad mProb
     #if random.randint(1,100) <= mProb:
 
+    for y in range (len(generacion_i)):
 
-    return poblacion #Devolver la nueva poblacion (sin evaluar)
+        if random.randint(1,100) <= (mProb*100):
+            mutacion = random.randint(0, size-1)
+            generacion_i[y][mutacion] = 0 if generacion_i[y][mutacion] == 1 else 1
+
+
+    return generacion_i #Devolver la nueva poblacion (sin evaluar)
 
 def main():
-    
+
+    iterations = 1000
+
     pesos = [ 34, 45, 14, 76, 32 ] #Para 5 objetos
     precios = [ 340, 210, 87, 533, 112 ] #Para 5 objetos
     pesoMax = 100 #Peso máximo que se puede poner en la mochila. Para 5 objetos
@@ -57,11 +102,51 @@ def main():
     k = 3 #Tamaño torneo selector de padres
     cProb = 0.7 #Probabilidad de cruce
     mProb = 0.1 #Probabilidad de mutacion
+    results = []
+
+
 
     l=len(pesos)
+
+    time_average = 0
+
     ##Creamos n soluciones aleatorias que sean válidas
     poblacion = []
-    for i in range(nSoluciones):
+
+
+
+    for i in range(iterations):
+
+        aux_nsoluciones = nSoluciones
+
+        print("\n------------------" + str(rep) + "------------------")
+
+
+        for j in range(aux_nsoluciones):
+            s = []
+
+            for i in range (l):
+                s.append(random.randint(0, 1))
+
+            poblacion.append([s, evaluarSolucion(s, precios, pesos, pesoMax)])
+
+        generation_average, generation_best = 0,0
+
+        for i in range(len(poblacion)):
+            generation_average += poblacion[i][1]
+            if (poblacion[i][1] > generation_best):
+                generation_best = poblacion[i][1]
+
+        generation_average /= (len(poblacion))
+
+        #que coño es esto XD
+
+
+
+
+
+
+    for j in range(nSoluciones):
         objetos = list(range(l))
         solucion = []
         peso = 0
@@ -98,7 +183,7 @@ def main():
             data = [i]
             data = data + results[i]
             if (i == 0):
-                data += [timeAvg]
+                data += [time_average]
             file.write(",".join([str(e) for e in data]) + "\n")
 
 if __name__ == "__main__":
