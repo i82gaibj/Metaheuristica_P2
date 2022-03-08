4f972ecd7d49b7844e04bd522bdb75e080d3546f
Javier García Ibáñez <i82gaibj@uco.es>
2022-03-08 21:04:03 +0100

    Prueba Final

--- KnapsackProb.py
+++ KnapsackProb.py
@@ -76,7 +76,7 @@ def aplicarOperadoresGeneticos(poblacion, k, cProb, mProb):
 
 def main():
 
-    iterations = 10
+    iterations = 1000
 
     pesos = [ 34, 45, 14, 76, 32 ] #Para 5 objetos
     precios = [ 340, 210, 87, 533, 112 ] #Para 5 objetos
