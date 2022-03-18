101bf3728bbbfa9f86ed2f7cdad9b4b8d77a8964
Javier García Ibáñez <i82gaibj@uco.es>
2022-03-18 23:22:07 +0100

    u

--- KnapsackElite.py
+++ KnapsackElite.py
@@ -107,7 +107,7 @@ def aplicarOperadoresGeneticos(poblacion, k, cProb, mProb,elite,precios,pesos,pe
 
 def main():
 
-    iterations = 10
+    iterations = 1
 
     #pesos = [ 34, 45, 14, 76, 32 ] #Para 5 objetos
     #precios = [ 340, 210, 87, 533, 112 ] #Para 5 objetos
@@ -211,7 +211,7 @@ def main():
     #print(" ")
     #print("El vector results guarda: ")
     for i in range(len(results)):
-            results[i][0] /= repeticiones
+            results[i][0] /= 1
             #print("Posicion ", i, " = ", results[i])
             
     
