3fb4780e20df16db6324905486b90a1bccca0e1c
Javier García Ibáñez <i82gaibj@uco.es>
2022-03-18 23:39:21 +0100

    u

--- KnapsackElite.py
+++ KnapsackElite.py
@@ -118,7 +118,7 @@ def main():
     pesoMax = 400 #Peso máximo que se puede poner en la mochila. Para 24 objetos
     
     nSolucionesInicial = 100 #Tamaño de la poblacion Default 25
-    maxGeneraciones = 5000 #Numero de generaciones Default 5
+    maxGeneraciones = 100 #Numero de generaciones Default 5
     k = 9 #Tamaño torneo selector de padres Default 3
     cProb = 0.9 #Probabilidad de cruce Default 0.7
     mProb = 0.1 #Probabilidad de mutacion Default 0.1
